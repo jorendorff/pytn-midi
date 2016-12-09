2914c5082479c516ac7bd2a86f06c0767fb91af6
Jason Orendorff <jason.orendorff@gmail.com>
2016-12-09 11:39:43 -1000

    Add `ScaleTempo` transform.

--- transforms.py
+++ transforms.py
@@ -1,3 +1,7 @@
+from mido import (
+    MetaMessage
+)
+
 from configman import (
     Namespace,
     RequiredConfig
@@ -13,3 +17,19 @@ class PassThrough(RequiredConfig):
     def __call__(self, message):
         return message
 
+
+class ScaleTempo(RequiredConfig):
+    required_config = Namespace()
+    required_config.add_option(
+        name="factor",
+        default=2.0,
+        doc="Scale all tempo information by this number (>1.0 to speed up, <1.0 to slow down)"
+    )
+
+    def __init__(self, config):
+        self.config = config
+
+    def __call__(self, message):
+        if isinstance(message, MetaMessage) and message.type == "set_tempo":
+            message.tempo = int(message.tempo / self.config.factor)
+        return message
